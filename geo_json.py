33aea4d363499e61731be20d10c5c91a67cec9b9
Eric Schles <ericschles@gmail.com>
2015-09-01 10:45:47 -0400

    first commit

---
+++ geo_json.py
@@ -0,0 +1,30 @@
+import json
+states = [{
+    "type":"Feature",
+    "properties": {"party": "Republican"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-104.05, 48.99],
+            [-97.22,  48.98],
+            [-96.58,  45.94],
+            [-104.03, 45.94],
+            [-104.05, 48.99]
+        ]]
+    }
+}, {
+    "type": "Feature",
+    "properties": {"party": "Democrat"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-109.05, 41.00],
+            [-102.06, 40.99],
+            [-102.03, 36.99],
+            [-109.04, 36.99],
+            [-109.05, 41.00]
+        ]]
+    }
+}]
+
+print json.dumps(states)
